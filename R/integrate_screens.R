0d48f8521ff53d06ab189a378f9004cbafd91fa2
RescueGum <dominik.meise@protonmail.com>
2021-07-12 17:26:14 +0200

    fixing errors in integrate_screens. now reproduces correctly the exact same results as the manual analysis using individual scripts.

--- R/integrate_screens.R
+++ R/integrate_screens.R
@@ -11,28 +11,46 @@
 #' @param inputformat a character string indicating the format of the input
 #'   data. One of "CASPR" (default), or "MAGeCK".
 #' @param plot logical. Should summary plots be generated automatically?
+#' @param RRAexact logical. When `TRUE` the RRA algorithm uses their "exact"
+#'   method to correct the p-value. Otherwise Bonferroni correction is applied.
 #'
 #' @return data.frame
 #' @export
 integrate_screens <- function(datapath,
                               infofile,
                               inputformat = "CASPR",
-                              plot = TRUE) {
+                              plot = TRUE,
+                              RRAexact = TRUE) {
 
   files <- list.files(datapath, full.names = TRUE)
 
   # RRA ===============
   gene_lists <- generate_glist(files, infofile)
-  RRA <- RobustRankAggreg::aggregateRanks(glist = gene_lists, method = "RRA", exact = TRUE)  #TODO: rethink an option to force bonferroni instead of 'exact'
-
+  RRA <- RobustRankAggreg::aggregateRanks(glist = gene_lists, method = "RRA", exact = RRAexact)
   rownames(RRA) <- NULL
+
+  # RRA pvalue score correction
+  if (RRAexact){
+    if (min(RRA$Score) < 0){
+      warning('The exact p-value correction of the RRA produced negative values! Values are set to the next lowest value...\n
+              ("RRA exact p-value correction" can be numerically unstable for small number of screens)' )
+      RRA$Score[RRA$Score < 0] <- min(RRA$Score[RRA$Score > 0])
+    }
+  } else {
+    RRA$Score <- RRA$Score * length(gene_lists)
+    RRA$Score[RRA$Score > 1] <- 1
+  }
 
   # EBM ===============
   EMP_data <- generate_dmatrices(files, infofile)
 
   EMP_data_genenames <- EMP_data[["pval_data"]][1]
-  EMP_data_pval_cols <- EMP_data[["pval_data"]][-1]
-  EMP_data_lfc_cols <- EMP_data[["lfc_data"]][-1]
+  EMP_data_pval_cols <- data.matrix(EMP_data[["pval_data"]][-1])
+  # data frame with lfc values has to be transposed to be used as the EBM's data
+  # matrix. genes have to be the "m variables in rows" and the screens are the
+  # "n samples in columns", to assure correct calculation of the data
+  # distribution:
+  EMP_data_lfc_cols <- t(EMP_data[["lfc_data"]][-1])
 
   EBM_results <- data.frame(matrix(ncol = 2, nrow = 0))
   colnames(EBM_results) <- c("Name", "EBMscore")
@@ -60,7 +78,9 @@ integrate_screens <- function(datapath,
 
   names(data) <- c("Name", "RRA_pval", "eBM_pval")
 
-  data["HMP"] <- apply(data[2:3], MARGIN = 1, FUN = harmonicmeanp::p.hmp)  #TODO: investigate whether to set multilevel to FALSE or set L correctly
+  # combining p values using the harmonic mean method, but without automatically
+  # controlling the FWER in order to subsequently correct for FDR
+  data["HMP"] <- apply(data[2:3], MARGIN = 1, FUN = harmonicmeanp::p.hmp, multilevel = FALSE, L = 2)
   data["HMP_fdr"] <- stats::p.adjust(data$HMP, method = "fdr")
 
   utils::write.csv(data, "valuetable.csv", row.names = F)
